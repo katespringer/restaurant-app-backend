36160f03ac909bfdfee07cecc32594a13139d382
katespringer <springek@gmail.com>
2022-06-13 14:48:08 -0400

    commit

--- config/environments/production/database.js
+++ config/environments/production/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', 'ec2-54-157-16-196.compute-1.amazonaws.com'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'd940dlhhh3nput'),
+        database: env('DATABASE_NAME', 'default'),
         username: env('DATABASE_USERNAME', 'iyyiwqnpkdwhwh'),
         password: env('DATABASE_PASSWORD', '67bed643d84f44038dd4c46643877fe93a1fbe13385e1fd175af4f66b9a030b'),
       },

5d376b5d1710331a04befe08efad3a0e139890cd
katespringer <springek@gmail.com>
2022-06-13 14:29:12 -0400

    update

--- config/environments/production/database.js
+++ config/environments/production/database.js
@@ -1,17 +1,20 @@
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', '@ec2-54-157-16-196.compute-1.amazonaws.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'd940dlhhh3nput'),
-      username: env('DATABASE_USERNAME', 'iyyiwqnpkdwhwh'),
-      password: env('DATABASE_PASSWORD', '467bed643d84f44038dd4c46643877fe93a1fbe13385e1fd175af4f66b9a030b'),
-      ssl: {
-        rejectUnauthorized: false
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'ec2-54-157-16-196.compute-1.amazonaws.com'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'd940dlhhh3nput'),
+        username: env('DATABASE_USERNAME', 'iyyiwqnpkdwhwh'),
+        password: env('DATABASE_PASSWORD', '67bed643d84f44038dd4c46643877fe93a1fbe13385e1fd175af4f66b9a030b'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
-    debug: false,
   },
 });

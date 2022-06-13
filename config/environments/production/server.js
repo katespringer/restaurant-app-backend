c68814f9e064df803a01f8e3fd695a3f88b26293
katespringer <springek@gmail.com>
2022-06-13 12:20:14 -0400

    update

--- config/environments/production/server.js
+++ config/environments/production/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  url: env("MY_HEROKU_URL"),
+  url: env('APP_URL'),
   proxy: true,
   app: {
-    keys: env.array("APP_KEYS", ["+bnMqMhCI2P/0thk76m/TjHczIz+rRmh+yBOurVQViM=", "U+p+s6LL0lhS+ly180yttDt1+i6M201Cq9Em5UZnOdE="]),
+    keys: env.array('APP_KEYS')
   },
 });

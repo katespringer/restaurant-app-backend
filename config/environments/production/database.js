93b5b6cf97694b1f79feb46de9886cf69dc4db65
katespringer <springek@gmail.com>
2022-06-13 13:43:40 -0400

    update

--- config/environments/production/database.js
+++ config/environments/production/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
+      host: env('DATABASE_HOST', '@ec2-54-157-16-196.compute-1.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'd940dlhhh3nput'),
+      username: env('DATABASE_USERNAME', 'iyyiwqnpkdwhwh'),
+      password: env('DATABASE_PASSWORD', '467bed643d84f44038dd4c46643877fe93a1fbe13385e1fd175af4f66b9a030b'),
       ssl: {
         rejectUnauthorized: false
       },
